ee0e3e9a6b727ca9669a709f891b8ef5ae4f5521
Michalina <miskalina1999@gmail.com>
2020-11-22 19:21:10 +0100

    Hejka

--- app/src/main/java/com/example/covdown/ui/oddech/OddechFragment_ciezar.java
+++ app/src/main/java/com/example/covdown/ui/oddech/OddechFragment_ciezar.java
@@ -1,6 +1,5 @@
 package com.example.covdown.ui.oddech;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,28 +17,124 @@ public class OddechFragment_ciezar extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.oddech_fragment_ciezar);
-
-        TextView title = findViewById(R.id.textViewCiezar2);
+        setContentView(R.layout.poziomy_ciezar);
+        TextView title = findViewById(R.id.textViewCiezar9);
         title.setText(R.string.ciezartytul);
-        final ImageView ilustracjaKroku = findViewById(R.id.imageViewCiezar);
-        ilustracjaKroku.setImageResource(R.drawable.baza);
-        final TextView opis = findViewById(R.id.textViewCiezar);
-        opis.setText(R.string.ciezaropisk1);
-        Button krok = findViewById(R.id.buttonCiezar);
-        krok.setOnClickListener(new View.OnClickListener() {
+        Button lvl1 = findViewById(R.id.lvl1);
+        lvl1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                opis.setText(R.string.ciezaropisk2);
-                //ilustracjaKroku.setImageResource(R.drawable.ciezark2);
+                setContentView(R.layout.oddech_fragment_ciezar);
+                TextView title2 = findViewById(R.id.textViewCiezar2);
+                title2.setText(R.string.ciezartytul);
+                final ImageView ilustracjaKroku = findViewById(R.id.imageViewCiezar);
+                ilustracjaKroku.setImageResource(R.drawable.baza);
+                final TextView opis = findViewById(R.id.textViewCiezar);
+                opis.setText(R.string.ciezaropisk1);
+                Button krok = findViewById(R.id.buttonCiezar);
+                krok.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        opis.setText(R.string.ciezaropisk2);
+                        //ilustracjaKroku.setImageResource(R.drawable.ciezark2);
+                    }
+                });
+                ImageButton cofnij = findViewById(R.id.buttonCiezarBck);
+                cofnij.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        setContentView(R.layout.poziomy_ciezar);
+                    }
+                });
             }
         });
-        ImageButton cofnij = findViewById(R.id.buttonCiezarBck);
+        Button lvl2 = findViewById(R.id.lvl2);
+        lvl2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.oddech_fragment_ciezar);
+                TextView title2 = findViewById(R.id.textViewCiezar2);
+                title2.setText(R.string.ciezartytul);
+                final ImageView ilustracjaKroku = findViewById(R.id.imageViewCiezar);
+                ilustracjaKroku.setImageResource(R.drawable.baza);
+                final TextView opis = findViewById(R.id.textViewCiezar);
+                opis.setText(R.string.ciezar2opisk1);
+                Button krok = findViewById(R.id.buttonCiezar);
+                krok.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        opis.setText(R.string.ciezar2opisk2);
+                        //ilustracjaKroku.setImageResource(R.drawable.ciezark2);
+                    }
+                });
+                ImageButton cofnij = findViewById(R.id.buttonCiezarBck);
+                cofnij.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        setContentView(R.layout.poziomy_ciezar);
+                    }
+                });
+            }
+        });
+        Button lvl3 = findViewById(R.id.lvl3);
+        lvl3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.oddech_fragment_ciezar);
+                TextView title2 = findViewById(R.id.textViewCiezar2);
+                title2.setText(R.string.ciezartytul);
+                final ImageView ilustracjaKroku = findViewById(R.id.imageViewCiezar);
+                ilustracjaKroku.setImageResource(R.drawable.baza);
+                final TextView opis = findViewById(R.id.textViewCiezar);
+                opis.setText(R.string.ciezar3opisk1);
+                Button krok = findViewById(R.id.buttonCiezar);
+                krok.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        opis.setText(R.string.ciezar3opisk2);
+                        //ilustracjaKroku.setImageResource(R.drawable.ciezark2);
+                    }
+                });
+                ImageButton cofnij = findViewById(R.id.buttonCiezarBck);
+                cofnij.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        setContentView(R.layout.poziomy_ciezar);
+                    }
+                });
+            }
+        });
+        Button lvl4 = findViewById(R.id.lvl4);
+        lvl4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.oddech_fragment_ciezar);
+                TextView title2 = findViewById(R.id.textViewCiezar2);
+                title2.setText(R.string.ciezartytul);
+                final ImageView ilustracjaKroku = findViewById(R.id.imageViewCiezar);
+                ilustracjaKroku.setImageResource(R.drawable.baza);
+                final TextView opis = findViewById(R.id.textViewCiezar);
+                opis.setText(R.string.ciezar4opisk1);
+                Button krok = findViewById(R.id.buttonCiezar);
+                krok.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        opis.setText(R.string.ciezar4opisk2);
+                        //ilustracjaKroku.setImageResource(R.drawable.ciezark2);
+                    }
+                });
+                ImageButton cofnij = findViewById(R.id.buttonCiezarBck);
+                cofnij.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        setContentView(R.layout.poziomy_ciezar);
+                    }
+                });
+            }
+        });
+        ImageButton cofnij = findViewById(R.id.lvlback);
         cofnij.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+            public void onClick(View v) { finish(); }});
     }
 }
